bed9b1a410e0feafc74f54f86127e92966dd30f8
otis wilcox <otiswilcox@gmail.com>
2021-08-06 17:26:25 -0400

    added api to .env

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import GameList from "./components/GameList";
-
+import Search from "./components/Search";
 function App() {
   return (
     <div className="App">
       <Router>
+        <Search />
         <GameList />
       </Router>
     </div>
